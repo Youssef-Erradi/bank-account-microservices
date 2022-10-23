1fd5d83d05788af83fa96a51321ab35bd7814436
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-10-23 16:34:00 +0100

    add configuration annotations to main classes

--- bank-account-cmd-api/src/main/java/bank/account/cmd/api/BankAccountCmdApiApplication.java
+++ bank-account-cmd-api/src/main/java/bank/account/cmd/api/BankAccountCmdApiApplication.java
@@ -1,9 +1,20 @@
 package bank.account.cmd.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import bank.account.core.config.AxonConfig;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@Import({AxonConfig.class})
 public class BankAccountCmdApiApplication {
 
 	public static void main(String[] args) {
--- bank-account-query-api/src/main/java/bank/account/query/api/BankAccountQueryApiApplication.java
+++ bank-account-query-api/src/main/java/bank/account/query/api/BankAccountQueryApiApplication.java
@@ -1,9 +1,19 @@
 package bank.account.query.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import bank.account.core.config.AxonConfig;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import({AxonConfig.class})
 public class BankAccountQueryApiApplication {
 
 	public static void main(String[] args) {
