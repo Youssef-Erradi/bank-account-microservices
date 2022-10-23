3611a93ea3244bc14fa437662fd8da28e0be0d10
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-10-23 15:17:20 +0100

    add bank account cmd api

---
+++ bank-account-cmd-api/src/main/java/bank/account/cmd/api/BankAccountCmdApiApplication.java
@@ -0,0 +1,13 @@
+package bank.account.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCmdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountCmdApiApplication.class, args);
+	}
+
+}
---
+++ bank-account-cmd-api/src/test/java/com/bank/BankAccountCmdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountCmdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
