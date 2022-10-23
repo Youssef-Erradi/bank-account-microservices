89bdc01e492751420f00fc481cade7a07fe7a958
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-10-23 15:17:36 +0100

    add bank account query api

---
+++ bank-account-query-api/src/main/java/bank/account/query/api/BankAccountQueryApiApplication.java
@@ -0,0 +1,13 @@
+package bank.account.query.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountQueryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountQueryApiApplication.class, args);
+	}
+
+}
