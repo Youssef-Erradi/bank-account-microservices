177975e3f8bee97d268e4eac792d8ae53904c3bd
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-10-23 15:16:39 +0100

    add bank account core project

---
+++ bank-account-core/src/main/java/bank/account/core/BankAccountCoreApplication.java
@@ -0,0 +1,13 @@
+package bank.account.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountCoreApplication.class, args);
+	}
+
+}
---
+++ bank-account-core/src/test/java/bank/account/core/BankAccountCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.account.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
